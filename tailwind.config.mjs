b05d327f1bc97bb88c31a2e0dd8deb0f8b602ef8
Stian N <stiannordholm@gmail.com>
2024-02-22 12:37:50 +0100

    Add custom delirium colors to tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				deliriumblack:"#080708",
+				deliriumyellow:"#fdca40",
+				deliriumblue:"#3772ff",
+				deliriumred:"#df2935",
+				deleriumwhite:"#e6e8e6",
+
+			}
+		},
+		
 	},
 	plugins: [],
 }
