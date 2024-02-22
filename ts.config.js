9a4934208b4d9436c9b95d38a97615d91db3d9e9
Stian N <stiannordholm@gmail.com>
2024-02-22 12:21:49 +0100

    Add color palette to ts.config.js

--- ts.config.js
+++ ts.config.js
@@ -3,7 +3,13 @@ module.exports = {
     theme: {
       colors: {
         // Configure your color palette here
-        
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#080708',
+      yellow: '#Fdca40',
+      blue: '#3772ff',
+      red: '#df2935',
+      gray: '#e6e8e6',
       }
     }
   }
